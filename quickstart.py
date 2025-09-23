075469869348b0d0406a9f3aba95822c0c877958
Tristan Kirksey <kirkseytc@gmail.com>
2025-09-23 10:42:40 -0400

    quickstart example added

---
+++ quickstart.py
@@ -0,0 +1,9 @@
+# https://flask.palletsprojects.com/en/stable/quickstart/#
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
