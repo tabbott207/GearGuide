ffea8fc878dbefd6fc28706d78e078fedb3b1e45
Tristan Kirksey <kirkseytc@gmail.com>
2025-10-08 17:36:43 -0400

    routes.py quickfix

--- GearGuide/routes.py
+++ GearGuide/routes.py
@@ -1,37 +1,35 @@
 # functions for handling routing the user to it's requested webpage
 
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app
 
-app = Flask(__name__)
-
-@app.route('/home')
+@current_app.route('/home')
 def homePage():
     return render_template('homepage.html')
 
-@app.route('/login')
+@current_app.route('/login')
 def loginPage():
     return render_template('login.html')
 
-@app.route('/signup')
+@current_app.route('/signup')
 def signupPage():
     return render_template('signup.html')
 
-@app.route('/create-trip')
+@current_app.route('/create-trip')
 def createTripPage():
     return render_template('create-trip.html')
 
-@app.route('/trips')
+@current_app.route('/trips')
 def myTripsPage():
     return render_template('my-trips.html')
 
-@app.route('/profile')
+@current_app.route('/profile')
 def myProfilePage():
     return render_template('my-profile.html')
 
-@app.route('/friends')
+@current_app.route('/friends')
 def myFriendsPage():
     return render_template('my-friends.html')
 
-@app.route('/trips/<int:id>')
+@current_app.route('/trips/<int:id>')
 def viewTripPage(id):
     return render_template('view-trip.html')
