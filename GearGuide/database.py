0579b40cf37c267ef5e67d07269b0634e0223ba5
kirkseytc <kirkseytc@gmail.com>
2025-10-15 12:53:36 -0400

    EOD 10/15/25

--- GearGuide/database.py
+++ GearGuide/database.py
@@ -1,4 +1,5 @@
 # functions for interacting with the database
+
 import sqlite3
 from datetime import datetime
 
@@ -9,7 +10,7 @@ from models import Trip, User
 TOTAL_TABLES = 6
 
 def startDB():
-    con = sqlite3.connect('GearGuide/data.sqlite3')
+    con = sqlite3.connect('')
     cur = con.cursor()
     
     if len(cur.execute('SELECT name FROM sqlite_master').fetchall()) != TOTAL_TABLES:
@@ -23,15 +24,14 @@ def initializeDB(cur: sqlite3.Cursor, con: sqlite3.Connection):
     cur.execute('DROP TABLE IF EXISTS user')
     cur.execute('DROP TABLE IF EXISTS trip')
     cur.execute('DROP TABLE IF EXISTS friend_request')
-    cur.execute('DROP TABLE IF EXISTS friend_pairing')
+    cur.execute('DROP TABLE IF EXISTS friendships')
     cur.execute('DROP TABLE IF EXISTS trip_whitelist')
     cur.execute('DROP TABLE IF EXISTS pack_list_item')
 
     # Create tables
     cur.execute('CREATE TABLE user(userID INT, username VARCHAR(32), email TEXT, passwordHash TEXT, profilePicFilename TEXT)')
     cur.execute('CREATE TABLE trip(tripID INT, userID INT, name VARCHAR(96), locationZip INT, startDate DATE, endDate DATE)')
-    cur.execute('CREATE TABLE friend_request(user1ID INT, user2ID INT)')
-    cur.execute('CREATE TABLE friend_pairing(user1ID INT, user2ID INT)')
+    cur.execute('CREATE TABLE friend_request(user1ID INT, user2ID INT, status TEXT)')
+    cur.execute('CREATE TABLE friendships(user1ID INT, user2ID INT)')
     cur.execute('CREATE TABLE trip_whitelist(userID INT, tripID INT)')
     cur.execute('CREATE TABLE pack_list_item(idID INT, tripID INT, itemName TEXT, is_checked BOOLEAN)')
-
