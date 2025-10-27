2cd66f24ee5ca7e846802132748912832a2840e6
kirkseytc <kirkseytc@gmail.com>
2025-10-27 11:59:21 -0400

    additional setup

--- GearGuide/routes.py
+++ GearGuide/routes.py
@@ -1,35 +1,39 @@
 # functions for handling routing the user to it's requested webpage
+from GearGuide import app
+from flask import render_template
 
-from flask import Flask, render_template, current_app
+@app.route('/')
+def index():
+    return "index page"
 
-@current_app.route('/home')
+@app.route('/home')
 def homePage():
     return render_template('homepage.html')
 
-@current_app.route('/login')
+@app.route('/login')
 def loginPage():
     return render_template('login.html')
 
-@current_app.route('/signup')
+@app.route('/signup')
 def signupPage():
     return render_template('signup.html')
 
-@current_app.route('/create-trip')
+@app.route('/create-trip')
 def createTripPage():
     return render_template('create-trip.html')
 
-@current_app.route('/trips')
+@app.route('/trips')
 def myTripsPage():
     return render_template('my-trips.html')
 
-@current_app.route('/profile')
+@app.route('/profile')
 def myProfilePage():
     return render_template('my-profile.html')
 
-@current_app.route('/friends')
+@app.route('/friends')
 def myFriendsPage():
     return render_template('my-friends.html')
 
-@current_app.route('/trips/<int:id>')
+@app.route('/trips/<int:id>')
 def viewTripPage(id):
     return render_template('view-trip.html')
---
+++ gearguide.py
@@ -0,0 +1,1 @@
+from GearGuide import app
